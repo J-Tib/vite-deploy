9ee8e14494d01d3c22cccba2d54e679296e6108e
J-Tib <jhuntibayan03@gmail.com>
2023-09-22 14:37:48 +0800

    fix : Img Issue

--- src/templates/Home.tsx
+++ src/templates/Home.tsx
@@ -29,7 +29,7 @@ function Home( {name, profession, imgSrc} : HomeProps) {
             <ResBtn children="Download CV"/>
         </div>
           <div className="img-container">
-            <img src={`${'.' + imgSrc}`} alt=""/>
+            <img src={`.${imgSrc}`} alt=""/>
           </div>
       </div>
     </section>
