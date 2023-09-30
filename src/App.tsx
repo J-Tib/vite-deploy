43941b373c65c39b8f5661ad564ac790f468979a
J-Tib <jhuntibayan03@gmail.com>
2023-09-30 17:06:58 +0800

    added new dependencies : ScrollReveal

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,6 @@
+import React from 'react'
+import { useEffect } from 'react'
+import ScrollReveal from 'scrollreveal'
 import './App.css'
 import HeaderPage from './templates/HeaderPage'
 import Home from './templates/Home'
@@ -6,6 +9,7 @@ import AboutPage from './templates/AboutPage'
 import ServicesPage from './templates/ServicesPage'
 import PortfolioPage from './templates/PortfolioPage'
 import ContactPage from './templates/ContactPage'
+
 
 function App() {
   const items = ['Home','About','Services', 'Portfolio', 'Contact']
@@ -16,6 +20,22 @@ function App() {
     imgSrc: MyPic.toString()
   }
   
+  useEffect(() => {
+    function scroll () {
+        ScrollReveal({
+        reset: true,
+        distance: '50px',
+        duration: 2000,
+        delay: 300
+      })
+      ScrollReveal().reveal('.home', { origin: 'top' })
+      ScrollReveal().reveal(`.home.img-container, .about, .contact,
+      .services, .portfolio`, { origin: 'bottom' })
+    }
+
+    return scroll;
+  }, [])
+
   console.log(person.imgSrc);
 
   return (
